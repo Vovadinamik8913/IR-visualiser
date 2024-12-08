e5d33db5cb15a0a4902aade2370621b46ed30a9b
Odnostorontseva Evgenia <e.odnostorontseva@g.nsu.ru>
2024-12-08 18:29:03 +0700

    added syntax highlighting using highlight.js

--- front/src/components/TXTpart.js
+++ front/src/components/TXTpart.js
@@ -1,4 +1,9 @@
 import React, {useState} from 'react';
+import hljs from '../../node_modules/highlight.js/lib/core.js';
+import '../../node_modules/highlight.js/styles/a11y-light.css';
+import llvm from '../../node_modules/highlight.js/lib/languages/llvm.js';
+
+hljs.registerLanguage('llvm', llvm);
 
 const TXTpart = ({ title, content, onLineClick }) => {
     const [lineIndex, setLineIndex] = useState(null);
@@ -13,16 +18,22 @@ const TXTpart = ({ title, content, onLineClick }) => {
         <div className="window" >
             <h2>{title}</h2>
             {content ? (
-                <pre className="txt-win"> <code>
-                    {content.split('\n').map((line, index) => (
-                        <div key={index}
-                             onClick={() => handleLineClick(index)}
-                             style={{cursor: "pointer"}}
-                        >
-                            {index + 1}  {line}
-                        </div>
-                    ))}
-                </code> </pre>
+                <pre className="txt-win">
+                    {content.split('\n').map((line, index) => {
+                        // Подсвечиваем каждую строку отдельно
+                        const highlightedLine = hljs.highlight(line, {language: 'llvm'}).value;
+                        return (
+                            <div
+                                key={index}
+                                onClick={() => handleLineClick(index)}
+                                style={{display: 'flex'}}
+                                dangerouslySetInnerHTML={{
+                                    __html: `<span class="line-number">${index + 1}</span> ${highlightedLine}`,
+                                }}
+                            ></div>
+                        );
+                    })}
+                </pre>
             ) : (
                 <p>Загрузите файл .ll для отображения содержимого.</p>
             )}
