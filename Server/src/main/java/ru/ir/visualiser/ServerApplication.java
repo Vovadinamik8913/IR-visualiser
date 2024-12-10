7c945b2e190abddeb3c7c6fce4f6e2d210e9550b
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-12-10 22:49:39 +0700

    change config error

--- Server/src/main/java/ru/ir/visualiser/ServerApplication.java
+++ Server/src/main/java/ru/ir/visualiser/ServerApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.ir.visualiser.files.Config;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		Config.deserializeFromJson();
-		SpringApplication.run(ServerApplication.class, args);
+        try {
+            Config.deserializeFromJson();
+			SpringApplication.run(ServerApplication.class, args);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
 	}
 
 }
